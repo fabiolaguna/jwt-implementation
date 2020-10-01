e4ca52dd63fdbf15c87829839d600adcb91a3469
Fabio <fabiolaguna.94@gmail.com>
2020-10-01 03:02:44 -0300

    Authorization header in response and fixes in form submit

--- template/js/login.js
+++ template/js/login.js
@@ -1,22 +1,22 @@
 const url = "http://localhost:8080/login";
 const loginForm = document.querySelector("#loginForm");
-const userName = document.querySelector("#login");
-const pwd = document.querySelector("#password");
 
-loginForm.onsubmit = function(){
+loginForm.onsubmit = async function(event){
+    event.preventDefault();
+
     let user = {
-        userName: userName.value,
-        pwd: pwd.value,
+        userName: this.login.value,
+        pwd: this.password.value,
     }
 
-    login(user);
+    await login(user);
+    this.reset();
 }
 
 async function login(user){
     try {
         let response = await post(`${url}`, user);
     } catch(error) {
-        console.log((error));
         console.log(Error(error));
     }
 }
@@ -24,12 +24,12 @@ async function login(user){
 function post(url, user){
     return new Promise(function(resolved, rejected){
         let request = new XMLHttpRequest();
-        request.open("POST", url, false);
+        request.open("POST", url, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        
+
         request.onload = function(){
             if (request.status <= 300){
-                resolved(request.response.headers);
+                resolved(request.getResponseHeader('Authorization'));
             } else {
                 rejected(Error("Login failed"));
             }
